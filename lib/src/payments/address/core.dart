bfea3b566c8f9499edf82c91cbf2b1ff8be6ba6c
Rafael Saes <git@rafael.saes.dev>
2023-11-29 08:19:41 -0300

    feat: add addres type labels

--- lib/src/payments/address/core.dart
+++ lib/src/payments/address/core.dart
@@ -2,15 +2,44 @@ import '../script/script.dart';
 import '../../models/networks.dart';
 
 enum AddressType {
+  // deprecated address type
+  p2pk,
+
   p2pkh,
   p2wpkh,
-  p2pk,
   p2tr,
+
+  // made up for silent payments, doesn't actually exist
+  p2sp,
+
   p2wsh,
   p2wshInP2sh,
   p2wpkhInP2sh,
   p2pkhInP2sh,
-  p2pkInP2sh
+  p2pkInP2sh;
+
+  @override
+  String toString() {
+    String label = '';
+    switch (this) {
+      case AddressType.p2pkh:
+        label = 'Legacy (1...)';
+        break;
+      case AddressType.p2wpkh:
+        label = 'SegWit (bc1q...)';
+        break;
+      case AddressType.p2tr:
+        label = 'Taproot (bc1p...)';
+        break;
+      case AddressType.p2sp:
+        label = 'Silent Payments (sp1p...)';
+        break;
+      default:
+        label = 'Mainnet';
+        break;
+    }
+    return label;
+  }
 }
 
 abstract class BitcoinAddress {
